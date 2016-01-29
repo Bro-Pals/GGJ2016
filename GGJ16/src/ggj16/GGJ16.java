359fe3241d85148039fea2781cedae4bfbf22489
Kevin Prehn <kprehn01@gmail.com>
2016-01-29 17:24:10 -0600

    Setting up the project

---
+++ GGJ16/src/ggj16/GGJ16.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ggj16;
+
+import bropals.lib.simplegame.AWTGameWindow;
+import bropals.lib.simplegame.GameStateRunner;
+import bropals.lib.simplegame.GameWindow;
+import bropals.lib.simplegame.io.AssetManager;
+import java.io.File;
+
+/**
+ *
+ * @author Pants
+ */
+public class GGJ16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AssetManager assetManager = new AssetManager(new File("").getAbsoluteFile(), false);
+        GameWindow window = new AWTGameWindow("Our game", 800, 600);
+        
+        GameStateRunner runner = new GameStateRunner(window, assetManager);
+        
+    }
+    
+}
