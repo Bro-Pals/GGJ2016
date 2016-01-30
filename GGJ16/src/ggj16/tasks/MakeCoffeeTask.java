29dd3b5101609b9d961bd995abf8ad2beecec38d
Kevin Prehn <kprehn01@gmail.com>
2016-01-29 19:44:22 -0600

    First task

---
+++ GGJ16/src/ggj16/tasks/MakeCoffeeTask.java
@@ -0,0 +1,16 @@
+
+package ggj16.tasks;
+
+import ggj16.Task;
+
+/**
+ *
+ * @author Kevin
+ */
+public class MakeCoffeeTask extends Task {
+
+    @Override
+    public void key(int i, boolean bln) {
+    }
+
+}
