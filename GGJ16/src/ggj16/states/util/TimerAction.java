3152918106ca71f52688d6b9090aefe9633587db
RadioLemon <horsentp@gmail.com>
2016-01-30 18:03:09 -0600

    Made a timer class (Why didn't we do this earlier)

---
+++ GGJ16/src/ggj16/states/util/TimerAction.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ggj16.states.util;
+
+/**
+ *
+ * @author Jonathon
+ */
+public interface TimerAction {
+    public void intervalComplete();
+}
